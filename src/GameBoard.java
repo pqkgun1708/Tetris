8097e494af7d01c28055ddbf847a1e4655ff5441
Gun <52684784+pqkgun1708@users.noreply.github.com>
2022-04-24 13:44:28 +0700

    Update GameBoard.java

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -2,6 +2,16 @@ import javax.swing.JFrame;
 
 public class GameBoard extends JFrame {
     public GameBoard() {
-        
+        // add component to board game
+        this.add(new GameArea());
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new GameBoard().setVisible(true);
+            }
+        });
     }
 }
