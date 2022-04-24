1663754704d239debbf0581f04b086c841c12fff
Gun <52684784+pqkgun1708@users.noreply.github.com>
2022-04-24 13:44:23 +0700

    Update GameArea.java

--- src/GameArea.java
+++ src/GameArea.java
@@ -1,5 +1,10 @@
 import javax.swing.JPanel;
+import java.awt.Graphics;
 
-public class GameArea extends JPanel{
-    
+public class GameArea extends JPanel {
+    // Draw stuff on board
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.fillRect(0, 0, 50, 50);
+    }
 }
